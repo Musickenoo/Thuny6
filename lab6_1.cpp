7fefd50a33d21c32924dfc7fefdf0f4798a40c70
Thunyatorn Kongmoung <Thunyatorn1125@gmail.com>
2023-12-01 15:35:46 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    
+    bool x = true;
+    int number,even=0,odd=0 ;
+
+    while (x)
+    {
+        cout << "Enter an integer: ";
+        cin >> number;
+        if(number % 2 == 0 && number != 0){
+            even +=1;
+        }
+        if(number % 2 == 1 && number != 0){
+            odd +=1;
+        }
+        if(number ==0){
+            x = false;
+        }
+    }
+    
+    cout << "#Even numbers = "<<even<<endl;
+    cout << "#Odd numbers = "<<odd;
     return 0;
 }
